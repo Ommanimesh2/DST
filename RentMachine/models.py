86df29c107bdcc5fc0027f3b4240f5165af5d851
codeprakhar25 <prakharkhatri123@gmail.com>
2023-04-20 16:16:52 +0530

    Feat: Admin Done

--- RentMachine/models.py
+++ RentMachine/models.py
@@ -8,8 +8,6 @@ class Renting(models.Model):
     MachineDetails= models.CharField(max_length=200)
     Price= models.IntegerField(default="0")
     Contact=models.IntegerField(default="0")
-    user_id=models.ForeignKey(User , to_field='id', on_delete=models.CASCADE, unique=True,blank=True, null=True)
-    rentimage=models.ImageField(upload_to="RentMachine/thumb-img", blank=True, null=True,default="" )
     BookedStatus = models.BooleanField("BookedStatus", default=False)
     date = models.DateField(auto_now=False, blank=True) 
     def __str__(self):
